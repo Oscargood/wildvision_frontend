9d46468922d1d5668467ab15394e27186c5bd06b
Oscargood <132227415+Oscargood@users.noreply.github.com>
2024-10-15 09:43:49 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-from flask import Flask, jsonify, send_from_directory
-import os
-import re
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Directory paths
-
-# DIR = '~/Documents/Software/Python/wildvision_backend/'     # local mac
-DIR = '~/var/data/'     # deplyed to render
-
-ANIMAL_DIR = os.path.join(DIR,'static/animal/')
-WEATHER_DIR = os.path.join(DIR,'static/weather/')
-VEGETATION_FILE = os.path.join(DIR,'static/vegetation/vegetation_native.geojson')
-ANIMAL_LOCATION_FILE = os.path.join(DIR,'static/animal/red_deer_location.geojson')
-
-# Serve index.html
 @app.route('/')
 def home():
-    return send_from_directory('templates', 'index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
