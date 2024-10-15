9c929cfe61c4a8753be74e9d6b76448f4c6c5894
Oscargood <132227415+Oscargood@users.noreply.github.com>
2024-10-16 07:08:43 +1300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -122,7 +122,7 @@ const plotDataLayer = async (layerGroup, layerType, dateIndex, timeIndex) => {
 
     // Fetch the GeoJSON data
     try {
-        const res = await fetch(`/${filename}`);
+        const res = await fetch(`/data/${filename}`);  
         if (!res.ok) {
             console.warn(`File not found: ${filename}`);
             return;
